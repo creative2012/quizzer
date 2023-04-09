7fde65b9b735833bbae7c3f7ce24997c3599db81
Paul <creative2012@hotmail.com>
2023-04-09 21:59:06 +0100

    meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import GlobalStyles from "../components/GlobalStyles";
+import GlobalStyles from '../components/GlobalStyles';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='description' content='' />
+        <meta name='keywords' content='' />
+        <meta name='HandheldFriendly' content='True' />
+        <meta name='MobileOptimized' content='320' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+        <meta http-equiv='cleartype' content='on' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Pacifico&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Press+Start+2P&display=swap'
           rel='stylesheet'
         />
-       
-       <GlobalStyles />
+
+        <GlobalStyles />
       </Head>
       <body>
         <div id={'globalLoader'}>
