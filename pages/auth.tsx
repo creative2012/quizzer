ae7ad97cb9c70ae1dee5fcb0f3a250555f3a55d7
Paul <creative2012@hotmail.com>
2023-04-09 21:49:53 +0100

    video css addition

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -27,7 +27,8 @@ const videoOption = {
   muted: true,
   loop: true,
   src: '/assets/women2.webm',
-  playsinline: true
+  playsinline: true,
+  disablePictureInPicture: true
 };
 
 export default function Auth() {
@@ -35,7 +36,7 @@ export default function Auth() {
   return (
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.7, ease: 'easeInOut' }} className=' text-zinc-700 bg-white' >
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ delay: 0.4, duration: 0.7, ease: 'easeInOut' }} className='absolute top-0 h-full overflow-hidden' >
-          <video {...videoOption}></video>
+          <video {...videoOption} ></video>
         </motion.div>
         <header className='bg-transparent fixed top-8 left-8 Bebas  text-7xl text-white z-10'><h1>{'{ QUIZZER. }'}</h1></header>
         <section>
