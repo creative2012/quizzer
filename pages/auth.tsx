79f5582f53d41b8a4762eb950302cd2297b7366e
Paul <creative2012@hotmail.com>
2023-04-09 21:46:05 +0100

    added css for mobile

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -34,7 +34,7 @@ export default function Auth() {
  
   return (
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.7, ease: 'easeInOut' }} className=' text-zinc-700 bg-white' >
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ delay: 0.4, duration: 0.7, ease: 'easeInOut' }} className='absolute top-0' >
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ delay: 0.4, duration: 0.7, ease: 'easeInOut' }} className='absolute top-0 h-full overflow-hidden' >
           <video {...videoOption}></video>
         </motion.div>
         <header className='bg-transparent fixed top-8 left-8 Bebas  text-7xl text-white z-10'><h1>{'{ QUIZZER. }'}</h1></header>
