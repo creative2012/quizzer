52bffe9d4e8d782b0ea10303395665108d6b45d3
Paul <creative2012@hotmail.com>
2023-04-09 23:21:55 +0100

    hover sticky attempt at fix for mobile

--- components/quiz/Answers.tsx
+++ components/quiz/Answers.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
 interface AnswerProps {
@@ -11,9 +11,40 @@ interface AnswerProps {
 }
 
 const Answers: React.FC<AnswerProps> = ({ onClick, delay, answers, keyValue, start, gameOver }) => {
+  const handleClick = (answer: string) => {
+    onClick(answer);
+  };
+
+  useEffect(() => {
+    const buttonElement = document.getElementById('myButton');
+    buttonElement?.addEventListener('touchstart', handleTouchStart);
+    document.addEventListener('touchmove', handleTouchMove);
+    document.addEventListener('touchend', handleTouchEnd);
+    return () => {
+      buttonElement?.removeEventListener('touchstart', handleTouchStart);
+      document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, []);
+
+  const handleTouchStart = () => {
+    const buttonElement = document.getElementById('myButton');
+    buttonElement?.classList.add('touch-active');
+  };
+
+  const handleTouchMove = () => {
+    const buttonElement = document.getElementById('myButton');
+    buttonElement?.classList.remove('touch-active');
+  };
+
+  const handleTouchEnd = () => {
+    const buttonElement = document.getElementById('myButton');
+    buttonElement?.classList.remove('touch-active');
+  };
   if (!answers) {
     return null;
   }
+  
 
   const alphabet = ['A', 'B', 'C', 'D'];
   if (start && !gameOver) {
@@ -38,7 +69,7 @@ const Answers: React.FC<AnswerProps> = ({ onClick, delay, answers, keyValue, sta
           {answers.map((answer, index) => (
             <div
               key={alphabet[index] + keyValue}
-              onClick={() => onClick(answer)}
+              onClick={() => handleClick(answer)}
               className='group hover:bg-[#ff5722] cursor-pointer noTrans text-sm relative grid grid-cols-6 gap-2 items-center justify-center shadow-md p-4 rounded-md bg-[#455a64]  text-white hover:shadow-lg transition transform hover:scale-110'
             >
               <div className='font-bold'>{alphabet[index]}</div>
